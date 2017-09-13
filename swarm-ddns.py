14609c9ac8570dccb2fdda9f4630eb6b0ea6f282
root <akdv88@github.com>
2017-09-13 10:05:40 +0000

    Add event listener

--- swarm-ddns.py
+++ swarm-ddns.py
@@ -15,39 +15,65 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-s",help="list of ip address[es] of your swarm nodes. Format: ipaddress,..",required=True)
 parser.add_argument("-n",help="your dnserver[s] configuration. Format: \"{'name':{'ip':'youripaddress','key':'yourupdatekey}}\"",required=True)
 parser.add_argument("-d",help="your domain name (example.)",required=True)
-parser.add_argument("-u",help="update frequency in seconds",type=int,required=True)
 args = parser.parse_args()
 
 swnodes = args.s.split(',')
 dnservers = ast.literal_eval(args.n)
 domain = args.d
-ttl = args.u
 
 def docker_query():
-    conn = docker.from_env()
-    serv_pre = set()
-    while True:
-        serv_cur = set()
-        for service in conn.services.list():
-            if 'add.dns' in service.attrs['Spec']['Labels']:
-                serv_cur.add(service.attrs['Spec']['Labels']['add.dns'])
-        if serv_pre != serv_cur:
-            add = serv_cur.difference(serv_pre)
-            rm = serv_pre.difference(serv_cur)
-            if add:
-                print('ADD', add)
-                for svc in add:
+    svc_list = {}
+    try:
+        conn = docker.from_env()
+    except:
+        print("Error: No connection to docker socket!")
+# Initialization
+    for service in conn.services.list():
+        if 'add.dns' in service.attrs['Spec']['Labels']:
+            svc = service.attrs['Spec']['Labels']['add.dns']
+            svc_list[service.attrs['Spec']['Name']] = svc
+            print('\nService/Action:', service.attrs['Spec']['Name']+'/add(update)')
+            try:
+                dns_add(svc.replace('_','-').lower())
+            except:
+                print("Error: DNS update failed!")
+# Event Listener
+    for event in conn.events('','','','true'):
+        svc_id = event['Actor']['ID']
+        if event['Type'] == 'service' and (event['Action'] == 'create' or event['Action'] == 'update') and 'add.dns' in conn.services.get(svc_id).attrs['Spec']['Labels']:
+            svc_name = conn.services.get(svc_id).attrs['Spec']['Name']
+            svc = conn.services.get(svc_id).attrs['Spec']['Labels']['add.dns']
+            print('\nService/Action:', svc_name+'/'+event['Action'])
+            if event['Action'] == 'create' or (event['Action'] == 'update' and svc_name not in svc_list):
+                try:
                     dns_add(svc.replace('_','-').lower())
-            if rm:
-                print('DEL', rm)
-                for svc in rm:
-                    dns_remove(svc.replace('_','-').lower())
-        serv_pre = serv_cur
-        sleep(ttl)
+                except:
+                    print("Error: DNS update failed!")
+            elif event['Action'] == 'update' and svc_name in svc_list:
+                svc_old = svc_list[svc_name]
+                try:
+                    dns_remove(svc_old.replace('_','-').lower())
+                except:
+                    print("Error: DNS update failed!")
+                try:
+                    dns_add(svc.replace('_','-').lower())
+                except:
+                    print("Error: DNS update failed!")
+            svc_list[svc_name] = svc
+        elif event['Type'] == 'service' and event['Action'] == 'remove':
+            svc_name = event['Actor']['Attributes']['name']
+            svc = svc_list[svc_name]
+            svc_list.pop(svc_name)
+            print(svc_list)
+            print('\nService/Action:', svc_name+'/'+event['Action'])
+            try:
+                dns_remove(svc.replace('_','-').lower())
+            except:
+                print("Error: DNS update failed!")
 
 def dns_add(svc):
     for host, conf in dnservers.items():
-        print('Add DNS Record \''+svc+'\' sent to',host,'dnserver ('+conf['ip']+')')
+        print('Add/Update DNS Record \''+svc+'\' sent to',host,'dnserver ('+conf['ip']+')')
         keyring = dns.tsigkeyring.from_text({
                 'rndc-key.' : conf['key']
                 })
@@ -55,6 +81,7 @@ def dns_add(svc):
         for swip in swnodes:
             update.add(svc, 15, 'a', swip)
             resp = dns.query.tcp(update, conf['ip'])
+
 def dns_remove(svc):
     for host, conf in dnservers.items():
         print('Remove DNS Record \''+svc+'\' sent to',host,'dnserver ('+conf['ip']+')')
@@ -66,4 +93,9 @@ def dns_remove(svc):
         resp = dns.query.tcp(update, conf['ip'])
 
 if __name__ == "__main__":
-    docker_query()
+    try:
+        docker_query()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('\nScript soft exit')
